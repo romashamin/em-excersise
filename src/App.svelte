cf9746bcebb77a22c336d499fd3779a93c32026e
Roman Shamin <roma@evilmartians.com>
2025-07-24 16:08:06 +0100

    Add PostHog integration

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,13 @@
 <script>
+  import posthog from 'posthog-js';
   let count = 0;
+
+  function handleClick() {
+    count++;
+    posthog.capture('button_clicked', { count });
+  }
 </script>
 
-<button on:click={() => count++}>
+<button on:click={handleClick}>
   Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import posthog from 'posthog-js';
+
+const POSTHOG_API_KEY = import.meta.env.VITE_POSTHOG_API_KEY;
+
+posthog.init(POSTHOG_API_KEY, {
+  api_host: 'https://us.i.posthog.com',
+  defaults: '2025-05-24',
+  person_profiles: 'identified_only',
+});
 
 const app = mount(App, {
   target: document.getElementById('app'),
