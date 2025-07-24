a4d75854b1441358f129a861594b35c80fbca546
Roman Shamin <roma@evilmartians.com>
2025-07-24 17:04:57 +0100

    Add Hotjar integration

--- src/main.js
+++ src/main.js
@@ -11,6 +11,23 @@ posthog.init(POSTHOG_API_KEY, {
   person_profiles: 'identified_only',
 });
 
+const HOTJAR_ID = import.meta.env.VITE_HOTJAR_ID;
+const HOTJAR_VERSION = import.meta.env.VITE_HOTJAR_VERSION;
+
+if (HOTJAR_ID && HOTJAR_VERSION) {
+  (function(h,o,t,j,a,r){
+      // @ts-ignore
+      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+      // @ts-ignore
+      h._hjSettings={hjid: HOTJAR_ID, hjsv: HOTJAR_VERSION};
+      a=o.getElementsByTagName('head')[0];
+      r=o.createElement('script');r.async=1;
+      // @ts-ignore
+      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+      a.appendChild(r);
+  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+}
+
 const app = mount(App, {
   target: document.getElementById('app'),
 })
